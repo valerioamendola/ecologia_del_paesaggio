8a9e59c94b1052b3c33e9efd90e4b7af8b7ed54d
valerioamendola <62288829+valerioamendola@users.noreply.github.com>
2020-03-25 12:54:24 +0100

    Create R_code_spatial2.r

---
+++ R_code_spatial2.r
@@ -0,0 +1,52 @@
+# R spatial
+
+# libreria sp
+
+library(sp)
+
+# dati da utilizzare
+
+data(meuse)
+
+head(meuse)
+
+#coordinate del dataset
+
+coordinates(meuse)=~x+y
+
+# spplot dei dati zinco
+
+spplot(meuse,"zinc")
+
+#spplot del rame
+
+# per vedere il nome si può usare names o head
+
+spplot(meuse,"copper")
+
+# bubble
+
+bubble(meuse,"zinc")
+
+# bubble del rame colorato di rosso
+
+bubble(meuse,"copper",col="red
+
+# foraminiferi (sofia), carbon capture)
+#array
+
+foram<-c(10, 20, 35, 55, 67, 80)
+
+carbon <- c(5, 15, 30, 70, 85, 99)
+
+lot(foram,carbon,col="green",cex=2,pch=19)
+
+#dati dall'esterno covid-19
+
+# cartella da creare su Windows c:/lab
+
+setwd("c:/lab")
+
+# funzione per leggere la tabella
+
+covid<-read.table("covid_agg.csv",head=TRUE)
