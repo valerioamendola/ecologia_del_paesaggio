47166396e63e85e4920f672ec532307492e16c28
valerioamendola <62288829+valerioamendola@users.noreply.github.com>
2020-05-28 11:26:31 +0200

    Update R_code_spatial.r

--- R_code_spatial.r
+++ R_code_spatial.r
@@ -13,17 +13,23 @@ plot(cadmium,lead,col="red",pch=19,cex=2)
 plot(copper,zinc,col="green",pch=17,cex=2)
 
 # per cambiare etichetta
+
 plot(copper,zinc,col="green",pch=17,cex=2,xlab="rame",ylab="zinc")
 
 #multiframe o multipanel
 
 par(mfrow=c(1,2))
+
 plot(cadmium,lead,col="red",pch=19,cex=2)
+
 plot(copper,zinc,col="green",pch=17,cex=2)
 
 #invertiamo riga colonna in colonna riga
+
 par(mfrow=c(2,1))
+
 plot(cadmium,lead,col="red",pch=19,cex=2)
+
 plot(copper,zinc,col="green",pch=17,cex=2)
 
 #multiframe automatico
